09b2a3589bb888993b90990f84bc88442c594b3f
Arnot001 <tarnot79@gmail.com>
2026-09-09 11:17:44 +0100

    Close TikTok Shop entry point cleanly

--- tiktok_shop_ui.py
+++ tiktok_shop_ui.py
@@ -5,4 +5,5 @@ from commerce.tiktok.shop_ui import open_shop_window
 root = tk.Tk()
 root.withdraw()
 window = open_shop_window(root)
+window.protocol("WM_DELETE_WINDOW", root.destroy)
 root.mainloop()
